032e11da312533dd05d0a4dd0498508587c1b5e6
FernandesVincent <vincent.fernandes@ensea.fr>
2025-12-05 09:37:42 +0100

    seance 3 STM32 part

--- STM32/STM32F446RE/Core/Src/main.c
+++ STM32/STM32F446RE/Core/Src/main.c
@@ -1,35 +1,37 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "can.h"
+#include "gpio.h"
 #include "i2c.h"
+#include "stm32f4xx_hal_gpio.h"
 #include "stm32f4xx_hal_uart.h"
 #include "usart.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdio.h>
-#include "echo.h"
 #include "BMP280.h"
 #include "MPU9250.h"
+#include "echo.h"
+#include <stdio.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -51,7 +53,7 @@
 
 /* USER CODE BEGIN PV */
 extern UART_HandleTypeDef huart2;
-char rx[100];
+// char rx[100];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -62,52 +64,63 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int _read(int file, char *ptr, int len)
-{
-    uint8_t c;
-    int count = 0;
+// int _read(int file, char *ptr, int len) {
+//   uint8_t c;
+//   int count = 0;
 
-    while (count < len)
-    {
-        HAL_UART_Receive(&huart2, &c, 1, HAL_MAX_DELAY);
+//   while (count < len) {
+//     HAL_UART_Receive(&huart2, &c, 1, HAL_MAX_DELAY);
 
-        // Echo vers le terminal
-        HAL_UART_Transmit(&huart2, &c, 1, HAL_MAX_DELAY);
+//     // // Echo vers le terminal
+//     // HAL_UART_Transmit(&huart2, &c, 1, HAL_MAX_DELAY);
 
-        // Convertir \r en \n (standard)
-        if (c == '\r')
-        {
-            c = '\n';
-            HAL_UART_Transmit(&huart2, (uint8_t*)"\n", 1, HAL_MAX_DELAY);
+//     // Convertir \r en \n (standard)
+//     if (c == '\r') {
+//       c = '\n';
+//       // HAL_UART_Transmit(&huart2, (uint8_t *)"\n", 1, HAL_MAX_DELAY);
+//     }
+
+//     *ptr++ = c;
+//     count++;
+
+//     if (c == '\n') // FIN DE LIGNE
+//       break;
+//   }
+
+//   return count;
+// }
+
+uint8_t rx;
+char cmd_buffer[50];
+int cmd_index = 0;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+      HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+        
+        if (rx != 0x00) {
+            if (rx != '\n' && rx != '\r' && cmd_index < sizeof(cmd_buffer) - 1) {
+                cmd_buffer[cmd_index++] = rx;
+            } else if (cmd_index > 0) { 
+                cmd_buffer[cmd_index] = '\0';
+                Rasbpi_protocol(cmd_buffer);
+                cmd_index = 0;
+            }
         }
 
-        *ptr++ = c;
-        count++;
+        HAL_UART_Transmit(&huart2, (uint8_t *)cmd_buffer, cmd_index, HAL_MAX_DELAY);
+        HAL_UART_Receive_IT(&huart1, &rx, 1);
 
-        if (c == '\n')    // FIN DE LIGNE
-            break;
     }
-
-    return count;
 }
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-
-  if(huart->Instance == USART1){
-
-    HAL_UART_Receive_IT(&huart1, (uint8_t *)rx, 1);
-    HAL_UART_Transmit(&huart1, (uint8_t *)rx, 1, HAL_MAX_DELAY);
-    Rasbpi_protocol(rx);
-  }
-}
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -115,7 +128,8 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -135,51 +149,51 @@ int main(void)
   MX_I2C1_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
-  HAL_UART_Receive_IT(&huart1, (uint8_t *)rx, 1);
+  HAL_UART_Receive_IT(&huart1, &rx, 1);
 
   /* USER CODE BEGIN 2 */
   printf("Demarrage du systeme\r\n");
-/////////////////////////////////////////////////////////////////////
-//   // Initialisation du BMP280
-//   BMP280_Init();
+  /////////////////////////////////////////////////////////////////////
+    // Initialisation du BMP280
+    BMP280_Init();
 
-//   // Vérification de l'ID du capteur (devrait retourner 0x58)
-//   BMP280_read_register(0xD0);  // Registre ID, pas 0x88
+    // Vérification de l'ID du capteur (devrait retourner 0x58)
+    BMP280_read_register(0xD0);  // Registre ID, pas 0x88
 
-//   // Configuration du capteur
-//   // 0x57 = 01010111b
-//   // bits 7-5 (010) : température oversampling x2
-//   // bits 4-2 (101) : pression oversampling x16  
-//   // bits 1-0 (11)  : mode normal
-//   BMP280_write_register(0xF4, 0x57);
+    // Configuration du capteur
+    // 0x57 = 01010111b
+    // bits 7-5 (010) : température oversampling x2
+    // bits 4-2 (101) : pression oversampling x16
+    // bits 1-0 (11)  : mode normal
+    BMP280_write_register(0xF4, 0x57);
 
-//   // Configuration du registre config (0xF5) - optionnel mais recommandé
-//   // 0xA0 = 10100000b
-//   // bits 7-5 (101) : standby 1000ms
-//   // bits 4-2 (000) : filter off
-//   // bit 0 (0)      : SPI désactivé
-//   BMP280_write_register(0xF5, 0xA0);
+    // Configuration du registre config (0xF5) - optionnel mais recommandé
+    // 0xA0 = 10100000b
+    // bits 7-5 (101) : standby 1000ms
+    // bits 4-2 (000) : filter off
+    // bit 0 (0)      : SPI désactivé
+    BMP280_write_register(0xF5, 0xA0);
 
-//   // Attendre que le capteur effectue une mesure
-//   // Temps de mesure typique = ~40ms avec ces paramètres
-//   HAL_Delay(100);
+    // Attendre que le capteur effectue une mesure
+    // Temps de mesure typique = ~40ms avec ces paramètres
+    HAL_Delay(100);
 
-//   // Lecture des valeurs brutes
-//   int raw_temp = BMP280_read_raw_temp();
-//   int raw_press = BMP280_read_raw_pressure();
+    // Lecture des valeurs brutes
+    // int raw_temp = BMP280_read_raw_temp();
+    // int raw_press = BMP280_read_raw_pressure();
 
-//   // Calcul des valeurs compensées
-//   // IMPORTANT : Il faut d'abord calculer la température car elle met à jour t_fine
-//   int compensate_temp = bmp280_compensate_T_int32(raw_temp); 
-//   int compensate_press = bmp280_compensate_P_int64(raw_press);
+    // Calcul des valeurs compensées
+    // IMPORTANT : Il faut d'abord calculer la température car elle met à jour t_fine 
+    // int compensate_temp = bmp280_compensate_T_int32(raw_temp);
+    // int compensate_press = bmp280_compensate_P_int64(raw_press);
 
-//   // Conversion en unités lisibles
-//   int temp_celsius = compensate_temp / 100.0f;  // Résolution 0.01°C
-//   int press_pa = compensate_press / 256.0f;     // Format Q24.8
-//   int press_hpa = press_pa / 100;               // Conversion en hPa
-//   printf("Temperature compensee: %d °C\r\n", temp_celsius);
-//   printf("Pression compensee: %d hPa\r\n", press_hpa);
-// /////////////////////////////////////////////////////////////////////
+    // Conversion en unités lisibles
+    // int temp_celsius = compensate_temp / 100.0f;  // Résolution 0.01°C
+    // int press_pa = compensate_press / 256.0f;     // Format Q24.8
+    // int press_hpa = press_pa / 100;               // Conversion en hPa
+    // printf("Temperature compensee: %d °C\r\n", temp_celsius);
+    // printf("Pression compensee: %d hPa\r\n", press_hpa);
+  // /////////////////////////////////////////////////////////////////////
 
   // int16_t ax, ay, az;
   // int16_t gx, gy, gz;
@@ -191,10 +205,9 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     // echo();
-    // BMP280_read_data_id();
+    //BMP280_read_data_id();
     // HAL_Delay(1000);
     // BMP280_read_data_id();
     // HAL_Delay(1000);
@@ -216,22 +229,21 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -242,22 +254,20 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 2;
   RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
     Error_Handler();
   }
 }
@@ -267,32 +277,30 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- STM32/STM32F446RE/TP1/echo/echo.c
+++ STM32/STM32F446RE/TP1/echo/echo.c
@@ -1,60 +1,53 @@
+#include "echo.h"
+#include "BMP280.h"
+#include "stm32f4xx_hal.h"
+#include "usart.h"
 #include <stdio.h>
 #include <string.h>
-#include "echo.h"
-#include "stm32f4xx_hal.h"
-#include "BMP280.h"
-#include "usart.h"
+#include <stdlib.h> 
 
-char message[100];
-char buffer[100];
+float K = 1.0f;  // int echo() {
+//   printf("Entrez un message: ");
+//   fgets(message, sizeof(message), stdin);
+//   printf("Vous avez entre: %s", message);
+//   return 0;
+// };
 
-int echo(){
-  printf("Entrez un message: ");
-  fgets(message, sizeof(message), stdin);
-  printf("Vous avez entre: %s", message);
-  return 0;
-};
+void Rasbpi_protocol(char *message) {
+  if (strcmp(message, "GET_T") == 0) {
+    printf("Commande GET_T recue\r\n");
+    int raw_temp = BMP280_read_raw_temp();
+    printf("T=%05XH\r\n", raw_temp & 0xFFFFF);
+    // int compensate_temp = bmp280_compensate_T_int32(raw_temp);
+    // int temp_celsius = compensate_temp / 100.0f;
+    // printf("Temperature compensee: %d °C\r\n", temp_celsius);
 
-void Rasbpi_protocol(char *message){
-      int i = 0;
-      while(*message != '\0' && *message != '\r' && *message != '\n' && i < sizeof(buffer)-1){
-          buffer[i++] = *message++;
-      }
+  } else if (strcmp(message, "GET_P") == 0) {
+    printf("Commande GET_P recue\r\n");
+    int raw_press = BMP280_read_raw_pressure();
+    printf("P=%05XH\r\n", raw_press & 0xFFFFF);
+    // int compensate_press = bmp280_compensate_P_int64(raw_press);
+    // int press_pa = compensate_press / 256.0f;     // Format Q24.8
+    // int press_hpa = press_pa / 100;               // Conversion en hPa
+    // printf("Pression compensee: %d hPa\r\n", press_hpa);
+  } else if (strcmp(message, "GET_K") == 0) {
+    printf("Commande GET_K recue\r\n");
+    printf("K=%10.5f\r\n", K);
 
-      buffer[i] = '\0';
+  } else if (strcmp(message, "GET_A") == 0) {
+    printf("Commande GET_A recue\r\n");
+    printf("A=000C8H\r\n");
 
-      if( strcmp(buffer, "GET_T") == 0 ){
-        printf("Commande GET_T recue\r\n");
-        int raw_temp = BMP280_read_raw_temp();
-        int compensate_temp = bmp280_compensate_T_int32(raw_temp); 
-        char hex[5];
-        sprintf(hex, "%X", compensate_temp);
-        char response[20];
-        sprintf(response, "T:%sH\r\n", hex);
-        HAL_UART_Transmit(&huart1, (uint8_t *)response, strlen(response), HAL_MAX_DELAY);
-      }
-      else if( strcmp(buffer, "GET_P") == 0 ){
-        printf("Commande GET_P recue\r\n");
-        int raw_press = BMP280_read_raw_pressure();
-        int compensate_press = bmp280_compensate_P_int64(raw_press);
-        char hex[5];
-        sprintf(hex, "%X", compensate_press);
-        char response[20];
-        sprintf(response, "P:%sH\r\n", hex);
-        HAL_UART_Transmit(&huart1, (uint8_t *)response, strlen(response), HAL_MAX_DELAY);
-      }
-      else if( strcmp(buffer, "GET_K") == 0 ){
+  } else if (strncmp(message, "SET_K=", 6) == 0) {
+    printf("Commande SET_K recue\r\n");
+    char *value_str = message + 6;
+    int value = atoi(value_str);   // Valeur entière reçue en 1/100e
+    K = value / 100.0f;            // Convertir en float
+    printf("K=OK\r\n");
+    printf("Nouvelle valeur de K: %10.5f\r\n", K);
 
-      }
-      else if( strcmp(buffer, "GET_A") == 0 ){
-
-      }
-      else if( strcmp(buffer, "SET_K=1234") == 0 ){
-
-      }
-
-      else {
-        printf("Commande inconnue: %s\r\n", buffer);
-      }
-
+  }
+  else {
+    printf("Commande inconnue: %s\r\n", message);
+  }
 }
